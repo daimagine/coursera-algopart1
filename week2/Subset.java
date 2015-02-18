ead28d0630d5b502af1ffe4478b7d3ed7e2c3d45
Adi Kurniawan <daimagine@gmail.com>
2015-02-19 05:33:24 +0700

    add week 2 Subset

---
+++ week2/Subset.java
@@ -0,0 +1,14 @@
+public class Subset {
+	public static void main(String[] args) {
+		RandomizedQueue<String> rq = new RandomizedQueue<String>();
+
+		while(!StdIn.isEmpty()) {
+			rq.enqueue(StdIn.readString());
+		}
+
+		int k = Integer.parseInt(args[0]);
+		for (int i = 0; i < k; i++) {
+			StdOut.println(rq.dequeue());
+		}
+	}
+}
